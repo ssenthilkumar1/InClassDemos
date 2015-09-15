9a6ec1ba4a86d854196bcaf0956aec3debde9078
Sudha Senthilkumar <ssenthilkumar1@studentmail.nait.ca>
2015-09-14 20:26:36 -0600

    create new VS project for class examples

---
+++ eRestaurantDemo/eRestaurant_Website/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eRestaurant_Website.Startup))]
+namespace eRestaurant_Website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
