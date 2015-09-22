bec4b23b8fbd5d30a712729070d81b028a63825a
Sudha Senthilkumar <ssenthilkumar1@studentmail.nait.ca>
2015-09-21 20:48:23 -0600

    Setting up the Component Library and Initial Entities

---
+++ eRestaurantDemo/eRestaurantSystem/DAL/Entities/Reservation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace eRestaurantSystem.DAL.Entities
+{
+    public class Reservation
+    {
+        [Key]
+        public int ReservationID { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime ReservationDate{get;set;}
+        public int NumberInParty { get; set; }
+        public string ContactPhone { get; set; }
+        public string ReservationStatus { get; set; }
+        public string EventCode { get; set; }
+
+        // Navigation Properties
+        public virtual SpecialEvent Event { get; set; }
+
+        // The Reservations table (sql) is a many to many
+        //relationship to the Table table (sql)
+
+        //SQL solves this problem by having an associate table
+        //that has a compound primary key created from Reservations
+        //and tables
+
+        //We will not be creating an entity for this associate Table.
+        //Instead we will create an overloaded Map in our DbContext class
+
+        //However, we can still create virtaul naviagtion property to accomodate
+        //this relationship
+
+
+        public virtual ICollection<Table> Tables { get; set; }
+
+    }
+}
---
+++ eRestaurantDemo/eRestaurantSystem/DAL/Entities/Table.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace eRestaurantSystem.DAL.Entities
+{
+    public class Table
+    {
+        [Key]
+        public int TableID { get; set; }
+        public byte TableNumber { get; set; } // tinyInt in SQL
+        public bool Smoking { get; set; }
+        public int Capacity { get; set; }
+        public bool Available { get; set; }
+
+
+        //Navigation Properties
+
+        // The Reservations table (sql) is a many to many
+        //relationship to the Table table (sql)
+
+        //SQL solves this problem by having an associate table
+        //that has a compound primary key created from Reservations
+        //and tables
+
+        //We will not be creating an entity for this associate Table.
+        //Instead we will create an overloaded Map in our DbContext class
+
+        //However, we can still create virtaul naviagtion property to accomodate
+        //this relationship
+
+
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
